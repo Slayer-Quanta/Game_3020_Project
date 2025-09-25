c3b075109e03bb53da1bc2428307ca15df3250e6
Slayer-Quanta <91901675+Slayer-Quanta@users.noreply.github.com>
2025-09-24 23:50:53 -0400

    Weapon firing and firing modes work
    Weapons can now fire, they can ricochet and we have alternate modes of fire

---
+++ Echelon/Assets/Player/Bullet.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            print("hit" + collision.gameObject.name + "!");
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            print("hit a wall");
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Echelon/Assets/Player/PlayerMovement.cs
+++ Echelon/Assets/Player/PlayerMovement.cs
@@ -32,6 +32,30 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -2f;
         }
 
-        float x = Input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical"); 
+
+        Vector3 move = transform.right * x + transform.forward *z;
+
+        controller.Move(move *  speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y =Mathf.Sqrt(jumpheight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (lastPosition != gameObject.transform.position && isGrounded == true)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;     
+        }
+        lastPosition = gameObject.transform.position;
     }
 }
